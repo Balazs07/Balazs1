707b6307e2c521075471823f506b7e02c3625189
Balázs Márton <tismma20balmar@innovationsgymnasiet.se>
2021-10-11 13:39:43 +0200

    Made a table to myDB, rename index.html to index.php, and deleted the old index.php

--- db_connection.php
+++ db_connection.php
@@ -15,4 +15,11 @@ function CloseCon($conn)
  $conn -> close();
  }
 
+//Select specific column from DB
+$query = mysqli_query($conn, "SELECT * FROM tbl_articles");
+while($row = mysqli_fetch_assoc($query))
+{
+  $title = $row['title'];
+  $content = $row['content'];
+}
 ?>
